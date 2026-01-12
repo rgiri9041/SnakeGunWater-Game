de674bb8e01787133eebe1cd27f1b71f486f51ec
Raj Kumar Giri <rgiri9041@gmail.com>
2026-01-12 10:21:35 -0600

    Increase the readability and proper output

--- main.py.py
+++ main.py.py
@@ -12,13 +12,16 @@ import random
 comp = random.randint(-1,1)
 youstr = input("Enter you choice (Gun, Water, Snake): ")
 youDict = {"g": 0, "w": 1, "s": -1}
+reverseDict = {0: "Gun", 1: "Water", -1: "Snake"}
 you = youDict[youstr]
+print(f"You chose {reverseDict[you]} \nComputer chose {reverseDict[comp]}")
 
 if(comp == you):
     print("Match Draw, Try Again!!")
 
 elif((comp == -1 and you == 0) or (comp == 0 and you == 1) or (comp == 1 and you == -1)):
     print("You Win")
-
+elif((you == -1 and comp == 0) or (you == 0 and comp == 1) or (you == 1 and comp == -1)):
+    print("Sorry You lose (:)")
 else:
-    print("Sorry. You Lose")
+    print("Something went wrong")
